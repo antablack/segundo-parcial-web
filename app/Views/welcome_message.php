10a18468e60b60a3303fe7af114a815e054c8e5d
Juan Sanchez <juan.sanchez@qrvey.com>
2021-05-03 23:02:33 -0500

    Add menu link

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -7,8 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <title>Sistema de gestion de transporte</title>
 </head>
@@ -18,9 +17,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Crud</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -28,17 +25,18 @@
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                         </li>
-                        
+
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Datos
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li>
-                                    <a class="dropdown-item"
-                                        href="<?php echo site_url('reparaciones/index') ?>">Reparaciones</a></li>
+                                    <a class="dropdown-item" href="<?php echo site_url('reparaciones/crear') ?>">Crear Reparaciones</a>
+                                </li>
                                 <li>
+                                    <a class="dropdown-item" href="<?php echo site_url('reparaciones/index') ?>">Reparaciones Reportes</a>
+                                </li>
                             </ul>
                         </li>
                     </ul>
@@ -54,21 +52,20 @@
 
     <div class="container">
         <!-- Reenderizar el contenido de todas las vistas -->
-        <?php 
-	if (isset($contenido)) {
-		//echo '<h1>' . $titulo . '</h1>';
-		echo view($contenido);
-	}else{
-	echo '<h5> Taller realizado por Kennedy Gamarra y Juan Sanchez</h5>';
-    }
-	?>
+        <?php
+        if (isset($contenido)) {
+            //echo '<h1>' . $titulo . '</h1>';
+            echo view($contenido);
+        } else {
+            echo '<h5> Taller realizado por Kennedy Gamarra y Juan Sanchez</h5>';
+        }
+        ?>
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
     </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
